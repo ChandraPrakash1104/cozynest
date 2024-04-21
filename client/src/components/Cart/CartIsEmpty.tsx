551ef0de5c439bab17f80941ba8bb968ea0da6eb
Chandra Prakash <chandraprakash1104@gmail.com>
2024-04-21 18:28:32 +0530

    Added empty cart page

---
+++ client/src/components/Cart/CartIsEmpty.tsx
@@ -0,0 +1,12 @@
+const CartIsEmpty = () => {
+  return (
+    <div
+      className='h-screen flex justify-center items-center'
+      style={{ height: '60vh' }}
+    >
+      Your Cart is Empty!!
+    </div>
+  );
+};
+
+export default CartIsEmpty;
--- client/src/components/pages/Cart.tsx
+++ client/src/components/pages/Cart.tsx
@@ -3,17 +3,23 @@ import Wrapper from '../common/UI/Wrapper';
 import OrderSummary from '../Cart/OrderSummary';
 import { useCart } from '../../hooks/useCart';
 import { Suspense, lazy, useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
+import { cartState } from '../../store/cart';
+import CartIsEmpty from '../Cart/CartIsEmpty';
 const CartItems = lazy(() => import('../Cart/CartItems'));
 
 const Cart = () => {
   const { loading } = useCart();
+  const cart = useRecoilValue(cartState);
+
   useEffect(() => {
     document.title = document.title.split(' ')[0] + ' | Cart';
   }, []);
+
   return (
     <div>
       {loading && <Loader />}
-      {!loading && (
+      {!loading && cart.length > 0 && (
         <Wrapper>
           <div className='py-12 w-full space-y-10 md:grid md:grid-cols-3'>
             <div className='col-span-2 md:pr-12 lg:pr-32'>
@@ -25,6 +31,7 @@ const Cart = () => {
           </div>
         </Wrapper>
       )}
+      {!loading && cart.length == 0 && <CartIsEmpty />}
     </div>
   );
 };
